7f507c59a2964ae2ac0e7b04ea4d53b0a360392d
Cedric Creusot <cedric.creusot@gmail.com>
2020-12-16 09:11:52 +0100

    feat(game screen): you can start a game. No end event for now.

--- lib/screens/simon_game_screen.dart
+++ lib/screens/simon_game_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
-
+import 'dart:async';
+import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter_simon/game/simon.dart';
+import 'package:flutter_simon/game/simon_state.dart' as state;
 
 class SimonGameScreen extends StatefulWidget {
   @override
@@ -10,11 +12,57 @@ class SimonGameScreen extends StatefulWidget {
 
 class _SimonGameScreenState extends State<SimonGameScreen> {
   Simon _simon;
+  Timer _gameLoopTimer;
+  String _message;
+  int _startingCountDown;
+  Stopwatch _elapsedTimeObserver;
+  bool _isPlayingColorSuit;
 
   @override
   void initState() {
     super.initState();
     _simon = Simon(Random());
+    _startingCountDown = 3;
+    _elapsedTimeObserver = Stopwatch();
+    _isPlayingColorSuit = false;
+    _message = "Ready";
+    _elapsedTimeObserver.start();
+    _gameLoopTimer = Timer.periodic(Duration(milliseconds: 500), (timer) {
+      if (_simon.state.shouldContinue) {
+        if (_simon.state.colorSuit.isEmpty && _startingCountDown >= 0) {
+          if (_elapsedTimeObserver.elapsed.inSeconds >= 1) {
+            setState(() {
+              if (_startingCountDown == 0) {
+                _message = "";
+              } else {
+                _message = "$_startingCountDown";
+              }
+              _startingCountDown--;
+            });
+            _elapsedTimeObserver.reset();
+          }
+        } else {
+          if (!_simon.state.isWaitingForInput && _startingCountDown <= 0) {
+            print("next Suit !");
+            setState(() {
+              _simon.saysColorSuitIs();
+              _isPlayingColorSuit = true;
+            });
+          } else {
+            setState(() {
+              _startingCountDown--;
+            });
+          }
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _gameLoopTimer.cancel();
+    _elapsedTimeObserver.stop();
+    super.dispose();
   }
 
   @override
@@ -24,59 +72,100 @@ class _SimonGameScreenState extends State<SimonGameScreen> {
         color: Colors.white,
         child: Hero(
           tag: "GamePad",
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Column(
-                children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: NoBoundSimonButton(
-                            color: Colors.red[400],
-                            pressedColor: Colors.red[800],
-                          ),
-                        ),
-                        Expanded(
-                          child: NoBoundSimonButton(
-                            color: Colors.green[400],
-                            pressedColor: Colors.green[800],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: NoBoundSimonButton(
-                            color: Colors.blue[400],
-                            pressedColor: Colors.blue[800],
-                          ),
-                        ),
-                        Expanded(
-                          child: NoBoundSimonButton(color: Colors.yellow[400], pressedColor: Colors.yellow[800]),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              ClipOval(child: Container(width: 192, height: 192, color: Colors.black))
-            ],
-          ),
+          child: (!_isPlayingColorSuit)
+              ? SimonGamePad(
+                  message: _message,
+                  onTap: (selectedColor) {
+                    setState(() {
+                      _simon.nextColorInSuitIS(selectedColor);
+                    });
+                  },
+                )
+              : SimonColorSuitPlayer(
+                  colorSuit: _simon.state.colorSuit,
+                  onEnded: () {
+                    setState(() {
+                      _isPlayingColorSuit = false;
+                      _startingCountDown = 6;
+                    });
+                  },
+                ),
         ),
       ),
     );
   }
 }
 
-class SimonColorSuitPlayer extends StatefulWidget {
-  final List<Color> _colorSuit;
+class SimonGamePad extends StatelessWidget {
+  final String message;
+  final void Function(state.Color selectedColor) onTap;
 
-  const SimonColorSuitPlayer({Key key, List<Color> colorSuit})
+  const SimonGamePad({Key key, this.message, this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: NoBoundSimonButton(
+                      color: Colors.red[400],
+                      pressedColor: Colors.red[800],
+                      onTap: () => onTap(state.Color.RED),
+                    ),
+                  ),
+                  Expanded(
+                    child: NoBoundSimonButton(
+                      color: Colors.green[400],
+                      pressedColor: Colors.green[800],
+                      onTap: () => onTap(state.Color.GREEN),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: NoBoundSimonButton(
+                      color: Colors.blue[400],
+                      pressedColor: Colors.blue[800],
+                      onTap: () => onTap(state.Color.BLUE),
+                    ),
+                  ),
+                  Expanded(
+                    child: NoBoundSimonButton(
+                      color: Colors.yellow[400],
+                      pressedColor: Colors.yellow[800],
+                      onTap: () => onTap(state.Color.YELLOW),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+        ClipOval(child: Container(width: 192, height: 192, color: Colors.black)),
+        Text(
+          message,
+          style: Theme.of(context).textTheme.bodyText1.copyWith(color: Colors.white),
+        )
+      ],
+    );
+  }
+}
+
+class SimonColorSuitPlayer extends StatefulWidget {
+  final List<state.Color> _colorSuit;
+  final void Function() onEnded;
+
+  const SimonColorSuitPlayer({Key key, List<state.Color> colorSuit, this.onEnded})
       : _colorSuit = colorSuit,
         super(key: key);
 
@@ -85,11 +174,81 @@ class SimonColorSuitPlayer extends StatefulWidget {
 }
 
 class _SimonColorSuitPlayerState extends State<SimonColorSuitPlayer> {
-  int _nextColorIndex = 0;
+  double _nextColorIndex = 0;
+  Timer _playerTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    print("Start Playing suit ${widget._colorSuit}");
+    _nextColorIndex = 0;
+    _playerTimer = Timer.periodic(Duration(milliseconds: 500), (timer) {
+      if (_nextColorIndex < widget._colorSuit.length - 1) {
+        setState(() {
+          _nextColorIndex += 0.5;
+        });
+      } else {
+        print("onEnd player");
+        widget.onEnded();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _playerTimer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    state.Color highlightColor =
+        (_nextColorIndex - _nextColorIndex.floor() == 0.5) ? null : widget._colorSuit[_nextColorIndex.floor()];
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: NoBoundSimonButton(
+                      color: (highlightColor == state.Color.RED) ? Colors.red[800] : Colors.red[400],
+                      pressedColor: Colors.red[800],
+                    ),
+                  ),
+                  Expanded(
+                    child: NoBoundSimonButton(
+                      color: (highlightColor == state.Color.GREEN) ? Colors.green[800] : Colors.green[400],
+                      pressedColor: Colors.green[800],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: NoBoundSimonButton(
+                      color: (highlightColor == state.Color.BLUE) ? Colors.blue[800] : Colors.blue[400],
+                      pressedColor: Colors.blue[800],
+                    ),
+                  ),
+                  Expanded(
+                    child: NoBoundSimonButton(
+                        color: (highlightColor == state.Color.YELLOW) ? Colors.yellow[800] : Colors.yellow[400],
+                        pressedColor: Colors.yellow[800]),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+        ClipOval(child: Container(width: 192, height: 192, color: Colors.black)),
+      ],
+    );
   }
 }
 
